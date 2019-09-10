2fb01eb077419e23c02da63106c65a51a83e89ad
Jie Zhu <jiezhu@h2644f3ca.dhcp.vt.edu>
2019-09-10 17:53:36 -0400

    lengthofthelongestsolution

---
+++ lengthofthelongestsolution.py
@@ -0,0 +1,16 @@
+def Lengthof(s):
+    dicts = {}
+    maxlength = start = 0
+    for i, value in enumerate(s):
+        if value in dicts:
+            sum = dicts[value] + 1
+            if sum > start:
+                start = sum
+        num = i - start + 1
+        if num > maxlength:
+            maxlength = num
+        dicts[value] = i
+    return maxlength
+
+
+print(Lengthof("abcabcdea"))
